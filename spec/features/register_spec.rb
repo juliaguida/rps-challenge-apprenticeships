e50c6d0ec28877d891f4de90d9498d8e4b1e419e
Julia.guida <32463353+juliaguida@users.noreply.github.com>
2022-01-18 09:43:09 +0000

    user storie 2 # As a marketeer # So that I can enjoy myself away from the daily grind # I would like to be able to play rock/paper/scissors

--- spec/features/register_spec.rb
+++ spec/features/register_spec.rb
@@ -1,5 +1,9 @@
 require 'spec_helper'
 
+#As a marketeer
+#So that I can see my name in lights
+#I would like to register my name before playing an online game
+
 feature 'register name' do
   scenario 'register and see my name' do
     visit ('/')
